ac17aff5e85be541bafa95ac03f3a61711a7085a
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2020-09-18 21:43:19 -0400

    react hooks useState

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,41 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
-    persons: [
-      { name: 'Max', age: 28, mood: "happy"},
-      { name: 'Manu', age: 29, mood: "sad"},
-      { name: 'Stephanie', age: 26, mood: "excited"}
-    ],
-    otherState: 'some other value'
-  }
-
-  switchNameHandler = () => {
-    // console.log('Was clicked!')
-    // DON'T DO THIS, WILL NOT WORK: this.state.person[0].name = 'Maximilian'
-    this.setState( {
+// Function that manages state
+const app = props => {
+    const [ personsState, setPersonsState ] = useState({
       persons: [
-        { name: 'Maximilian', age: 28, mood: "happy"},
-        { name: 'Manu', age: 29, mood: "happy"},
-        { name: 'Stephanie', age: 27, mood: "excited"}
-      ]
-    } )
-  }
+        { name: 'Max', age: 28, mood: "happy"},
+        { name: 'Manu', age: 29, mood: "sad"},
+        { name: 'Stephanie', age: 26, mood: "excited"}
+      ],
+      otherState: 'some other value'
+    });
+  
+    const [otherState, setOtherState] = useState('some other value');
 
-  render() {
+    console.log(personsState, otherState);
+//
+// Function that can have other functions do something
+    const switchNameHandler = () => {
+      // console.log('Was clicked!')
+      // DON'T DO THIS, WILL NOT WORK: this.state.person[0].name = 'Maximilian'
+      setPersonsState( {
+        persons: [
+          { name: 'Maximilian', age: 28, mood: "thicc"},
+          { name: 'Manu', age: 29, mood: "happy"},
+          { name: 'Stephanie', age: 27, mood: "litty"}
+        ],
+        otherState: personsState.otherState
+      } )
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} mood={this.state.persons[0].mood} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} mood={this.state.persons[1].mood}>Manu's hobbies are crying to sad rom com movies :C</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} mood={this.state.persons[2].mood}/>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person 
+          name={personsState.persons[0].name}
+          age={personsState.persons[0].age} 
+          mood={personsState.persons[0].mood} 
+        />
+        <Person 
+          name={personsState.persons[1].name} 
+          age={personsState.persons[1].age} 
+          mood={personsState.persons[1].mood}
+        >Manu's hobbies are crying to sad rom com movies :C
+        </Person>
+        <Person 
+          name={personsState.persons[2].name} 
+          age={personsState.persons[2].age} 
+          mood={personsState.persons[2].mood}
+        />
       </div>
     );
-  }
 }
 
-export default App;
+export default app;
+
