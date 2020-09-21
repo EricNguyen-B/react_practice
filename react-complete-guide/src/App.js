a13bf3c6625b075e70dbacb8b08f7761a2ec916c
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2020-09-21 11:08:23 -0400

    component mini-project

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,73 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Person from './Person/Person';
+import Output from './Output/Output';
 
-// Function that manages state
-const app = props => {
-    const [ personsState, setPersonsState ] = useState({
-      persons: [
-        { name: 'Max', age: 28, mood: "happy"},
-        { name: 'Manu', age: 29, mood: "sad"},
-        { name: 'Stephanie', age: 26, mood: "excited"}
-      ],
-      otherState: 'some other value'
-    });
-  
-    const [otherState, setOtherState] = useState('some other value');
-
-    console.log(personsState, otherState);
-//
-// Function that can have other functions do something
-    const switchNameHandler = (newName) => {
-      // console.log('Was clicked!')
-      // DON'T DO THIS, WILL NOT WORK: this.state.person[0].name = 'Maximilian'
-      setPersonsState( {
-        persons: [
-          { name: newName, age: 28, mood: "thicc"},
-          { name: 'Manu', age: 29, mood: "happy"},
-          { name: 'Stephanie', age: 27, mood: "litty"}
-        ],
-        otherState: personsState.otherState
-      } )
-    }
-
-    const nameChangedHandler = (event) => {
-      setPersonsState( {
-        persons: [
-          { name: 'Max', age: 28, mood: "thicc"},
-          { name: event.target.value, age: 29, mood: "happy"},
-          { name: 'Stephanie', age: 27, mood: "litty"}
-        ],
-        otherState: personsState.otherState
-      } )
-    }
-  
-
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button onClick={switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button>
-        <Person 
-          name={personsState.persons[0].name}
-          age={personsState.persons[0].age} 
-          mood={personsState.persons[0].mood} 
-        />
-        <Person 
-          name={personsState.persons[1].name} 
-          age={personsState.persons[1].age} 
-          mood={personsState.persons[1].mood}
-          click={switchNameHandler.bind(this, 'Max!!')}
-          changed={nameChangedHandler} > Manu's hobbies are crying to sad rom com movies :C 
-        </Person>
-        <Person 
-          name={personsState.persons[2].name} 
-          age={personsState.persons[2].age} 
-          mood={personsState.persons[2].mood} 
-        />
-      </div>
-    );
+// UserInput component
+const App = () => {
+  return(
+    <div className="App">
+      <h1>Form</h1>
+      <input type="text"></input>
+      <Output username="Sam" />
+      <Output username="Dan" />
+      <Output username="Kevin" />
+    </div>
+  );
 }
 
-export default app;
+export default App;
+
+
+
 
