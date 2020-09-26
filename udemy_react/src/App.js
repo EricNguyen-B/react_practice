a69a929a0ab5efc21c3657564b09f93104c1400b
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2020-09-26 17:34:14 -0400

    updated structure and added odin project course

--- udemy_react/src/App.js
+++ udemy_react/src/App.js
@@ -14,14 +14,21 @@ class App extends Component {
   }
 
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    } )
+  nameChangedHandler = (event, id ) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.setState.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons} )
   }
 
   deletePersonHandler = (personIndex) => {
@@ -53,7 +60,9 @@ class App extends Component {
                 return <Person 
                   click={() => this.deletePersonHandler(index)}
                   name={person.name} 
-                  age={person.age} />
+                  age={person.age} 
+                  key={person.id}
+                  changed={(event) => this.nameChangedHandler(event, person.id)}/>
               })}
         </div> 
       );
