60643aea04c3da4e07fbcf9084057c7cb25618b5
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2020-09-26 16:47:59 -0400

    updated deletePersonHandler

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
   }
 
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons;
+    const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({persons: persons});
   }
