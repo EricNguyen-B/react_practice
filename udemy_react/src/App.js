2dc3d71ab4d3fc2637c1b7c51f55b124707da235
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2020-09-28 18:27:36 -0400

    react styling-components

--- udemy_react/src/App.js
+++ udemy_react/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
+
 import Person from './Person/Person';
 
 class App extends Component {
@@ -92,7 +92,6 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
         <div className="App">
           <h1>Hi, I'm a React App</h1>
           <p className={classes.join(' ')}>This is really working!</p>
@@ -101,10 +100,8 @@ class App extends Component {
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
         </div>
-      </StyleRoot>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default Radium(App);
+export default App;
--- udemy_react/src/Person/Person.js
+++ udemy_react/src/Person/Person.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import Radium from 'radium';
+import styled from 'styled-components';
 
-import './Person.css';
+// import './Person.css';
+const StyledDiv = styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+
+    @media (min-width: 500px) {
+        width: 450px;
+    }
+`;
+
 
 const person = ( props ) => {
     const style = {
@@ -10,12 +23,13 @@ const person = ( props ) => {
         }
     };
     return (
-        <div className="Person" style={style}>
+        // <div className="Person" style={style}>
+        <StyledDiv>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        </div>
+        </StyledDiv>
     )
 };
 
-export default Radium(person);
+export default person;
