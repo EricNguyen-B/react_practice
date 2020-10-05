8d7c5337c10e65b88fb1864600ebe5d3bc7328b1
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2020-10-05 19:59:29 -0400

    lifecycle hooks

--- udemy_react/src/containers/App.js
+++ udemy_react/src/containers/App.js
@@ -7,6 +7,11 @@ import Cockpit from '../components/Cockpit/Cockpit';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       { id: 'asfa1', name: 'Max', age: 28 },
@@ -15,6 +20,15 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  } 
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
   }
 
   nameChangedHandler = ( event, id ) => {
@@ -49,6 +63,7 @@ class App extends Component {
   }
 
   render () {
+    console.log('[App.js] render');
     let persons = null;
 
     if ( this.state.showPersons ) {
@@ -58,12 +73,13 @@ class App extends Component {
             clicked={this.deletePersonHandler}
             changed={this.nameChangedHandler}
             />
-    }
+    } 
 
 
     return (
         <div className={classes.App}>
           <Cockpit 
+          title={this.props.appTitle}
           showPersons={this.state.showPersons} 
           persons={this.state.persons} 
           clicked={this.togglePersonsHandler}/>
